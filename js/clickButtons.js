a2855def65309116c6228b847624f6970cf9aa04
Ariel Parra <arielemilio_parra@hotmail.com>
2024-03-29 09:21:07 -0600

    fix click buttons timeout

--- js/clickButtons.js
+++ js/clickButtons.js
@@ -2,16 +2,16 @@ function clickButtons() {
     const appEstructuraCurso = document.querySelector("app-estructura-curso");
     if (appEstructuraCurso) {
         const buttons = appEstructuraCurso.querySelectorAll('button.mat-focus-indicator.mat-button.mat-button-base');
-        for (let i = 0; i < buttons.length; i++) {
-            /* TODO: find why this doesnt end */
-            const button = buttons[i];
-            if (!button.textContent.includes("Examen")) {
-                button.click();
-            }
-            if(i==buttons.length-1){
-                return;
+        async function clickButtonAtIndex(index) {
+            if (index < buttons.length) {
+                if (!buttons[index].textContent.includes("Examen")) {
+                    buttons[index].click();
+                    await new Promise(resolve => setTimeout(resolve, 100)); 
+                }
+                await clickButtonAtIndex(index + 1); 
             }
         }
+        clickButtonAtIndex(0);
     } else {
         console.log('No exam found on this page.');
     }
